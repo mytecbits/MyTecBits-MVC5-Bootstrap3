e413f92b00da2c7d697416ca4531c042cfe5b2a0
My Tec Bits <info@mytecbits.com>
2013-10-19 22:45:07 -0500

    Bootstrap 3 with MVC 5

---
+++ MyTecBits-MVC5-Bootstrap3/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace MyTecBits_MVC5_Bootstrap3
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ MyTecBits-MVC5-Bootstrap3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MyTecBits_MVC5_Bootstrap3.MvcApplication" Language="C#" %>
